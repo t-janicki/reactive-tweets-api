b674c6eef95e573a7966b4f550666163286fde5f
tjaygallery <tjaygallery@gmail.com>
2020-03-16 12:27:01 +0100

    chat

--- src/main/java/com/webflux/config/WebSocketConfiguration.java
+++ src/main/java/com/webflux/config/WebSocketConfiguration.java
@@ -1,33 +1,48 @@
 package com.webflux.config;
 
-import com.webflux.comment.event.CustomWebsocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 @Configuration
-class WebSocketConfiguration {
+@EnableWebSocketMessageBroker
+class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Bean
     Executor executor() {
         return Executors.newSingleThreadExecutor();
     }
 
-    @Bean
-    HandlerMapping handlerMapping(CustomWebsocketHandler wsh) {
-        return new SimpleUrlHandlerMapping() {
-            {
-                System.out.println("Handler mapping");
-                setUrlMap(Map.of("/ws/comments", wsh));
-                setOrder(10);
-            }
-        };
+//    @Bean
+//    HandlerMapping handlerMapping(CustomWebsocketHandler wsh) {
+//        return new SimpleUrlHandlerMapping() {
+//            {
+//                System.out.println("Handler mapping");
+//                setUrlMap(Map.of("/ws/comments", wsh));
+//                setOrder(10);
+//            }
+//        };
+//    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocketApp").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue/");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Bean
